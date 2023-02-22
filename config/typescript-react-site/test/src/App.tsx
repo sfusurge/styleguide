b18290667bbe441681bc18a30b968c9b0bf3dfbd
Ethan P <eth-p+git@hidden.email>
2023-02-21 18:08:49 -0800

    typescript-react-site: Add jsonc support

--- config/typescript-react-site/test/src/App.tsx
+++ config/typescript-react-site/test/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./App.css";
+import Data from "./test.jsonc";
 
 function App() {
 	return (
@@ -10,7 +11,7 @@ function App() {
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
+					Learn React. {Data.hello}
 				</a>
 			</header>
 		</div>
